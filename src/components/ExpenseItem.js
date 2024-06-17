3f1d3933d94bdbbb9f970e59396f6c2599daa4ab
Shubh-Sad-Ratna <shubhamband2000@gmail.com>
2024-06-17 14:49:54 +0530

    StylingComponent

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
-function ExpenseItem(){
-    return (
-        <>
-        <p>Food - $5 </p>
-        <p> Petrol - $20 </p>
-        <p> Movie - $10</p>
-        </>
-    )
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className='expense-item'>
+      <div className='expense-item__location'>15 August 2023</div>
+      <div className='expense-item__description'>
+        <h2>Book</h2>
+        <div className='expense-item__price'>$10</div>
+        <div>Delhi</div>
+      </div>
+    </div>
+  );
 }
+
 export default ExpenseItem;
